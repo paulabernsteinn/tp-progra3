089907d8590127e629edb77049dca3c0120a65b5
paulabernsteinn <bernsteinp@udesa.edu.ar>
2025-09-21 19:03:47 -0300

    buscador

--- tp3/src/Component/Buscador/Buscador.js
+++ tp3/src/Component/Buscador/Buscador.js
@@ -3,12 +3,23 @@ import React, { Component } from "react";
 class Buscador extends Component{
     constructor(props){
         super(props)
+          this.state = {
+            valor: "",
+  }
     }
+ evitarSubmit(event) {
+    event.preventDefault();
+  }
+
+controlarCambios(event) {
+    this.setState({valor: event.target.value});
+  }
 
     render(){
+
         return(
-              <form class="search-form" action="results.html" method="get">
-                <input type="text" class="" name="searchData" placeholder="Buscar..." value=""/>
+              <form class="search-form" action="results.html" method="get" onSubmit={(event)=>this.evitarSubmit(event)}>
+                <input type="text" class="" name="searchData" placeholder="Buscar..." onChange={(event)=>this.controlarCambios(event)} value={this.state.valor}/>
                 <button type="submit" class="btn btn-success btn-sm">Buscar</button>
             </form>
         )
@@ -16,32 +27,3 @@ class Buscador extends Component{
 }
 
 export default Buscador
-
-// import React, { Component } from "react";
-
-// class Buscador extends Component{
-//     constructor(props){
-//         super(props)
-//           this.state = {
-//             valor: "",
-//   }
-//     }
-//  evitarSubmit(event) {
-//     event.preventDefault();
-//   }
-
-// controlarCambios(event) {
-//     this.setState({valor: event.target.value});
-//   }
-
-//     render(){
-//         return(
-//               <form class="search-form" action="results.html" method="get" onSubmit={(event)=>this.evitarSubmit(event)}>
-//                 <input type="text" class="" name="searchData" placeholder="Buscar..." onChange={(event)=>this.controlarCambios(event)} value={this.state.valor}/>
-//                 <button type="submit" class="btn btn-success btn-sm">Buscar</button>
-//             </form>
-//         )
-//     }
-// }
-
-// export default Buscador
---
+++ tp3/src/Screens/DetalleSerie/DetalleSerie.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import Header from "../../Component/Header/Header";
+import Footer from "../../Component/Footer/Footer";
+
+class DetalleSerie extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            SerieDetalle: {}, 
+        }
+    }
+    componentDidMount(){
+        const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNGFkNTljMGNjMDA5MGE3MDFiZmFjYjg5M2Y2MmE4MyIsIm5iZiI6MTc1NzQ0OTc3Ny41NTksInN1YiI6IjY4YzA4ZTMxYmRiOGY3MzY2MjliZDNjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AE7116u_74D_hCFnsAHtj8OeB6PtVVgIVIU8VhMf_do'
+}
+};
+  const id = this.props.match.params.id
+
+fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, options)
+  .then(res => res.json())
+  .then(res => {
+    console.log(res)
+    
+    this.setState({
+        SerieDetalle: res
+    })
+})
+  .catch(err => console.error(err));
+}
+    render(){
+        return(
+             <div className="container">
+                <h1>UdeSA Movies</h1>
+       <Header/>
+             <section class="row cards all-movies" id="movies">
+             <h2 class="alert alert-primary">Detalle de las películas Populares</h2>
+             <DetalleSerieHijo SerieDetalle={this.state.SerieDetalle} />
+             </section>
+
+              <Footer />
+             </div>
+        )
+    }
+}
+// CAMBIAR URL E IMPORTAR DETALLESERIEHIJO
+
+export default DetalleSerie
